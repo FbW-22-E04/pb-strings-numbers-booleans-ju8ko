2c5212a960171cc821d6ce9caf909229ab25c38e
ju8ko <julia.x.kobzar@gmail.com>
2022-06-29 14:50:43 +0200

    Exercise is done

--- solution.js
+++ solution.js
@@ -1,1 +1,35 @@
+//1.
+const str = "String";
 
+//2.
+console.log("second element is", str[1]);
+console.log("6th element is", str[5]);
+
+//3. String value
+const firstName = "Julia";
+
+//4. Number value
+const age = 30;
+
+//5. Number value
+const year = 1992;
+
+//6.
+console.log(firstName + " is " + age + " in " + year);
+
+//7.
+const isMarried = true;
+
+//8.
+console.log(firstName + " is married: " + isMarried);
+
+//9.
+const programmingLanguage = "JavaScript";
+const isFun = true;
+console.log(programmingLanguage + " is fun: " + isFun);
+
+//10.
+console.log(programmingLanguage[0], programmingLanguage[4]);
+
+//11.
+console.log(programmingLanguage.length);
